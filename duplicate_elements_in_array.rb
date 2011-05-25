b8b27fd14915651d47dc1de2c7d0a95553923316
Cody Johnston <cody@metroenterprises.us>
2011-05-25 02:58:06 -0700

    burp

--- duplicate_elements_in_array.rb
+++ duplicate_elements_in_array.rb
@@ -1,11 +1,7 @@
-# ** BEGIN RAGE **
-# Hey rubyists! Have you ever had a stupid CS question asked of you during an interview,
-# well here's the solution to a common one ~>
-# ** END RAGE **
-
 # Given an array of n elements
 # with 1 duplicate entry
-# write a function that returns that array entry
+# write a function that
+# returns the duplicate array entry
 
 array = []
 100.times { |n| array << (n + 1) }
